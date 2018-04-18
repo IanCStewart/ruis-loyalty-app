7bab7cbacf40dbd3d330e8c0ff9dcd2ce3eca06f
Ian Stewart <ian.stewart@anchor.chat>
2018-04-18 10:12:40 +0200

    Fixed linting errors.

--- App.test.js
+++ App.test.js
@@ -1,9 +1,9 @@
+/* eslint-env mocha */
 import React from 'react';
-import App from './App';
-
 import renderer from 'react-test-renderer';
+import App from './App';
 
 it('renders without crashing', () => {
   const rendered = renderer.create(<App />).toJSON();
-  expect(rendered).toBeTruthy();
+  expect(rendered).toBeTruthy(); // eslint-disable-line no-undef
 });
