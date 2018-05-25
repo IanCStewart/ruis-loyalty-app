69fb9f6199d8e6ff29bd5713c4c1036d07811f13
Ian Stewart <ian.stewart@anchor.chat>
2018-05-25 14:44:36 +0200

    Added theme color to activeTintColor.

--- app/navigators/app.js
+++ app/navigators/app.js
@@ -5,12 +5,19 @@ import Qr from '../screens/qr';
 import Deals from '../screens/deals';
 import Profile from '../screens/profile';
 
-const Tabs = TabNavigator({
-  Home: { screen: Home },
-  Events: { screen: Events },
-  QR: { screen: Qr },
-  Deals: { screen: Deals },
-  Profile: { screen: Profile }
-});
+const Tabs = TabNavigator(
+  {
+    Home: { screen: Home },
+    Events: { screen: Events },
+    QR: { screen: Qr },
+    Deals: { screen: Deals },
+    Profile: { screen: Profile }
+  },
+  {
+    tabBarOptions: {
+      activeTintColor: '#4B77BE'
+    }
+  }
+);
 
 export default Tabs;
