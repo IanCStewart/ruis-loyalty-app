0821a63132eac721918fa6f23165d85b6c4cdf22
Ian Stewart <ian.stewart@anchor.chat>
2018-06-22 10:36:25 +0200

    Added bottom margin to card component.

--- app/components/card/styles.js
+++ app/components/card/styles.js
@@ -4,6 +4,7 @@ export default theme => StyleSheet.create({
   container: {
     marginLeft: 29,
     marginRight: 29,
+    marginBottom: 16,
     height: 190,
     backgroundColor: theme.colors.darkGray,
     borderRadius: 8
