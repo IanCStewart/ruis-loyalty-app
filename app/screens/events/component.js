105429261b6d27cb3733bc2a860436cb76927d4f
Ian Stewart <ian.stewart@anchor.chat>
2018-06-22 10:34:22 +0200

    Removed card component from events page.

--- app/screens/events/component.js
+++ app/screens/events/component.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { View, Text } from 'react-native';
 import style from './style';
 import IconEvents from '../../components/icons/event';
-import Card from '../../components/card';
 
 const navigationOptions = {
   tabBarIcon: ({ tintColor }) => <IconEvents color={tintColor} /> // eslint-disable-line react/display-name, react/prop-types, max-len
@@ -11,11 +10,6 @@ const navigationOptions = {
 const Events = () => (
   <View style={style.root}>
     <Text>Events</Text>
-    <Card
-      imageSource={{ uri: 'https://source.unsplash.com/1600x900/?drink' }}
-      title="Title"
-      subTitle="subtitle"
-    />
   </View>
 );
 
