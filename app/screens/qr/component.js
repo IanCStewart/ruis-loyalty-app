18f04e017155c481bf13702ad9519ee8fb1c237a
Ian Stewart <ian.stewart@anchor.chat>
2018-06-25 17:08:22 +0200

    Removed stack header for now.

--- app/screens/qr/component.js
+++ app/screens/qr/component.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { View, Text, Image } from 'react-native';
+import { SafeAreaView, Text, Image } from 'react-native';
+import IconQr from '../../components/icons/qr';
 import qrCode from '../../../assets/images/qr.png';
 import getStyles from './styles';
 
@@ -11,17 +12,22 @@ const propTypes = {
   }).isRequired,
 };
 
+const navigationOptions = {
+  tabBarIcon: ({ tintColor }) => <IconQr color={tintColor} /> // eslint-disable-line react/display-name, react/prop-types, max-len
+};
+
 const Qr = ({ theme }) => {
   const styles = getStyles(theme);
 
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <Text style={styles.pageTitle}>Persoonlijke Code</Text>
       <Image style={styles.image} source={qrCode} />
-    </View>
+    </SafeAreaView>
   );
 };
 
 Qr.propTypes = propTypes;
+Qr.navigationOptions = navigationOptions;
 
 export default Qr;
