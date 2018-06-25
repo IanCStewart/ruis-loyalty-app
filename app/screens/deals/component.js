13594394c3306727187185545b235fe5e6efd8e2
Ian Stewart <ian.stewart@anchor.chat>
2018-06-25 21:17:15 +0200

    WIP: Created deals screen.

--- app/screens/deals/component.js
+++ app/screens/deals/component.js
@@ -1,18 +1,104 @@
-import React from 'react';
-import { View, Text } from 'react-native';
-import style from './style';
-import IconDeals from '../../components/icons/deals';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import {
+  FlatList,
+  View,
+  Text,
+  ActivityIndicator,
+  Animated
+} from 'react-native';
+import getStyles from './styles';
+import Card from '../../components/card';
 
-const navigationOptions = {
-  tabBarIcon: ({ tintColor }) => <IconDeals color={tintColor} /> // eslint-disable-line react/display-name, react/prop-types, max-len
+const propTypes = {
+  getNews: PropTypes.func.isRequired,
+  getNewsSuccess: PropTypes.func.isRequired,
+  loading: PropTypes.bool.isRequired,
+  data: PropTypes.arrayOf(Object).isRequired,
+  navigation: PropTypes.shape({
+    navigate: PropTypes.func.isRequired,
+    setParams: PropTypes.func.isRequired
+  }).isRequired,
+  theme: PropTypes.shape({
+    colors: PropTypes.object.isRequired,
+    fonts: PropTypes.object.isRequired
+  }).isRequired,
 };
 
-const Deals = () => (
-  <View style={style.root}>
-    <Text>Deals</Text>
-  </View>
-);
+class Deals extends Component {
+  constructor(props) {
+    super(props);
 
-Deals.navigationOptions = navigationOptions;
+    this.headerOpacity = new Animated.Value(0);
+  }
+
+  static navigationOptions = ({ navigation }) => ({
+    headerStyle: {
+      opacity: !navigation.state.params ? 0 : navigation.state.params.headerOpacity,
+      backgroundColor: '#212121',
+      borderBottomWidth: 0
+    },
+    headerTransparent: true
+  });
+
+  componentDidMount() {
+    const { getNews, getNewsSuccess } = this.props;
+
+    this.props.navigation.setParams({
+      headerOpacity: this.headerOpacity.interpolate({
+        inputRange: [154, 166], outputRange: [0, 1]
+      })
+    });
+
+    getNews();
+    setTimeout(getNewsSuccess, 500);
+  }
+
+  onItemPress = item => this.props.navigation.navigate('Article', { article: item.title, item });
+
+  renderItem = ({ item, index }) => (
+    <Card
+      imageSource={{ uri: `https://source.unsplash.com/1600x900/?event,night-club,${index}` }}
+      title={item.title}
+      subTitle={item.subtitle}
+      onPress={() => this.onItemPress(item)}
+      buttonText="Lees verder"
+    />
+  )
+
+  render() {
+    const { data, loading, theme } = this.props;
+    const styles = getStyles(theme);
+
+    if (loading) {
+      return (
+        <View style={styles.activityIndicatorContainer}>
+          <ActivityIndicator animating={true}/>
+        </View>
+      );
+    }
+
+    const AnimatedFlatlist = Animated.createAnimatedComponent(FlatList);
+
+    return (
+      <AnimatedFlatlist
+        ListHeaderComponent={<Text style={styles.heading}>Deals</Text>}
+        data={data}
+        renderItem={this.renderItem}
+        keyExtractor={(item, index) => `item-${index}`}
+        style={styles.container}
+        onScroll={
+          Animated.event(
+            [{ nativeEvent: { contentOffset: { y: this.headerOpacity } } }],
+            { useNativeDriver: true }
+          )
+        }
+        scrollEventThrottle={16}
+      />
+    );
+  }
+}
+
+Deals.propTypes = propTypes;
 
 export default Deals;
--- app/screens/deals/container.js
+++ app/screens/deals/container.js
@@ -1,4 +1,22 @@
 import { connect } from 'react-redux';
+import compose from 'recompose/compose';
+import WithTheme from 'anchor-ui-native/with-theme';
 import Deals from './component';
+import { getNews, getNewsSuccess } from '../../actions/news';
 
-export default connect()(Deals);
+const mapStateToProps = state => ({
+  loading: state.news.loading,
+  data: state.news.data
+});
+
+const mapDispatchToProps = dispatch => ({
+  getNews: () => dispatch(getNews()),
+  getNewsSuccess: () => dispatch(getNewsSuccess())
+});
+
+const enhance = compose(
+  WithTheme,
+  connect(mapStateToProps, mapDispatchToProps)
+);
+
+export default enhance(Deals);
