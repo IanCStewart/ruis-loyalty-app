412d8643c058db33b8b827350b4fc7995afb2063
Ian Stewart <ian.stewart@anchor.chat>
2018-06-25 08:39:55 +0200

    Adjusted header animation on home screen.

--- app/screens/home/component.js
+++ app/screens/home/component.js
@@ -47,7 +47,7 @@ class Home extends Component {
 
     this.props.navigation.setParams({
       headerOpacity: this.headerOpacity.interpolate({
-        inputRange: [34, 46], outputRange: [0, 1]
+        inputRange: [154, 166], outputRange: [0, 1]
       })
     });
 
